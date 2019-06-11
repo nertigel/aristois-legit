f500f675e047e9d6471681a3a806758a9b205778
designer1337 <damiankorczak38@gmail.com>
2019-06-11 16:30:15 +0200

    hook draw_set_color

--- core/hooks/hooks.cpp
+++ core/hooks/hooks.cpp
@@ -29,7 +29,7 @@ hooks::reset_fn original_reset;
 HWND hooks::window;
 WNDPROC hooks::wndproc_original = NULL;
 
-void hooks::initialize() {
+void hooks::initialize() noexcept {
 	client_hook = std::make_unique<vmt_hook>();
 	clientmode_hook = std::make_unique<vmt_hook>();
 	panel_hook = std::make_unique<vmt_hook>();
@@ -56,7 +56,8 @@ void hooks::initialize() {
 
 	surface_hook->setup(interfaces::surface);
 	surface_hook->hook_index(67, reinterpret_cast<void*>(lock_cursor));
-	surface_hook->hook_index(116, reinterpret_cast<void*>(on_screen_size_changed));
+	surface_hook->hook_index(116, reinterpret_cast<void*>(on_screen_size_changed));	
+	surface_hook->hook_index(15, reinterpret_cast<void*>(draw_set_color));
 
 	modelrender_hook->setup(interfaces::model_render);
 	modelrender_hook->hook_index(21, reinterpret_cast<void*>(draw_model_execute)); //hooked for backtrack chams
@@ -86,7 +87,7 @@ void hooks::initialize() {
 }
 
 
-void hooks::shutdown() {
+void hooks::shutdown() noexcept {
 	clientmode_hook->release();
 	client_hook->release();
 	panel_hook->release();
@@ -102,7 +103,7 @@ void hooks::shutdown() {
 	SetWindowLongW(FindWindowW(L"Valve001", NULL), GWL_WNDPROC, reinterpret_cast<LONG>(wndproc_original));
 }
 
-float __stdcall hooks::viewmodel_fov() {
+float __stdcall hooks::viewmodel_fov() noexcept {
 	auto local_player = reinterpret_cast<player_t*>(interfaces::entity_list->get_client_entity(interfaces::engine->get_local_player()));
 
 	if (local_player && local_player->is_alive()) {
@@ -113,7 +114,35 @@ float __stdcall hooks::viewmodel_fov() {
 	}
 }
 
-void __stdcall hooks::on_screen_size_changed(int old_width, int old_height) {
+void __stdcall hooks::draw_set_color(int r, int g, int b, int a) noexcept {
+	static auto original_fn = reinterpret_cast<draw_set_color_fn>(surface_hook->get_original(15));
+
+	auto color_red = config_system.item.clr_crosshair[0] * 255;
+	auto color_green = config_system.item.clr_crosshair[1] * 255;
+	auto color_blue = config_system.item.clr_crosshair[2] * 255;
+	auto color_alpha = config_system.item.clr_crosshair[3] * 255;
+
+	auto outline_red = config_system.item.clr_crosshair_outline[0] * 255;
+	auto outline_green = config_system.item.clr_crosshair_outline[1] * 255;
+	auto outline_blue = config_system.item.clr_crosshair_outline[2] * 255;
+	auto outline_alpha = config_system.item.clr_crosshair_outline[3] * 255;
+
+	if (config_system.item.crosshair_color) {
+		static const auto crosshair_color_fn = utilities::pattern_scan(GetModuleHandleA("client_panorama.dll"), "FF 50 3C 80 7D 20 00") + 3;
+		if (_ReturnAddress() == reinterpret_cast<void*>(crosshair_color_fn))
+			return original_fn(interfaces::surface, color_red, color_green, color_blue, color_alpha);
+	}
+
+	if (config_system.item.crosshair_outline_color) {
+		static const auto crosshair_outline_color_fn = utilities::pattern_scan(GetModuleHandleA("client_panorama.dll"), "FF 50 3C F3 0F 10 4D ? 66 0F 6E C6") + 3;
+		if (_ReturnAddress() == reinterpret_cast<void*>(crosshair_outline_color_fn))
+			return original_fn(interfaces::surface, outline_red, outline_green, outline_blue, outline_alpha);
+	}
+
+	original_fn(interfaces::surface, r, g, b ,a);
+}
+
+void __stdcall hooks::on_screen_size_changed(int old_width, int old_height) noexcept {
 	static auto original_fn = reinterpret_cast<on_screen_size_changed_fn>(surface_hook->get_original(116));
 
 	original_fn(interfaces::surface, old_width, old_height);
@@ -121,7 +150,7 @@ void __stdcall hooks::on_screen_size_changed(int old_width, int old_height) {
 	render.setup_fonts();
 }
 
-int __stdcall hooks::do_post_screen_effects(int value) {
+int __stdcall hooks::do_post_screen_effects(int value) noexcept {
 	static auto original_fn = reinterpret_cast<do_post_screen_effects_fn>(clientmode_hook->get_original(44));
 
 	visuals.glow();
@@ -129,7 +158,7 @@ int __stdcall hooks::do_post_screen_effects(int value) {
 	return original_fn(interfaces::clientmode, value);
 }
 
-bool __stdcall hooks::create_move(float frame_time, c_usercmd* user_cmd) {
+bool __stdcall hooks::create_move(float frame_time, c_usercmd* user_cmd) noexcept {
 	static auto original_fn = reinterpret_cast<create_move_fn>(clientmode_hook->get_original(24));
 	auto local_player = reinterpret_cast<player_t*>(interfaces::entity_list->get_client_entity(interfaces::engine->get_local_player()));
 	original_fn(interfaces::clientmode, frame_time, user_cmd); //fixed create move
@@ -176,7 +205,7 @@ bool __stdcall hooks::create_move(float frame_time, c_usercmd* user_cmd) {
 	return false;
 }
 
-void __fastcall hooks::override_view(void* _this, void* _edx, c_viewsetup* setup) {
+void __fastcall hooks::override_view(void* _this, void* _edx, c_viewsetup* setup) noexcept {
 	static auto original_fn = reinterpret_cast<override_view_fn>(clientmode_hook->get_original(18));
 	auto local_player = reinterpret_cast<player_t*>(interfaces::entity_list->get_client_entity(interfaces::engine->get_local_player()));
 
@@ -187,7 +216,7 @@ void __fastcall hooks::override_view(void* _this, void* _edx, c_viewsetup* setup
 	original_fn(interfaces::clientmode, _this, setup);
 }
 
-void __stdcall hooks::draw_model_execute(IMatRenderContext * ctx, const draw_model_state_t & state, const model_render_info_t & info, matrix_t * bone_to_world) {
+void __stdcall hooks::draw_model_execute(IMatRenderContext * ctx, const draw_model_state_t & state, const model_render_info_t & info, matrix_t * bone_to_world) noexcept {
 	static auto original_fn = reinterpret_cast<draw_model_execute_fn>(modelrender_hook->get_original(21));
 
 	visuals.backtrack_chams(ctx, state, info);
@@ -196,7 +225,7 @@ void __stdcall hooks::draw_model_execute(IMatRenderContext * ctx, const draw_mod
 	original_fn(interfaces::model_render, ctx, state, info, bone_to_world);
 }
 
-void __stdcall hooks::frame_stage_notify(int frame_stage) {
+void __stdcall hooks::frame_stage_notify(int frame_stage) noexcept {
 	static auto original_fn = reinterpret_cast<frame_stage_notify_fn>(client_hook->get_original(37));
 	static auto backtrack_init = (backtrack.init(), false);
 
@@ -219,7 +248,7 @@ void __stdcall hooks::frame_stage_notify(int frame_stage) {
 
 	original_fn(interfaces::client, frame_stage);
 }
-void __stdcall hooks::paint_traverse(unsigned int panel, bool force_repaint, bool allow_force) {
+void __stdcall hooks::paint_traverse(unsigned int panel, bool force_repaint, bool allow_force) noexcept {
 	if (strstr(interfaces::panel->get_panel_name(panel), "HudZoom")) {
 		if (interfaces::engine->is_connected() && interfaces::engine->is_in_game()) {
 			if (config_system.item.remove_scope)
@@ -239,7 +268,7 @@ void __stdcall hooks::paint_traverse(unsigned int panel, bool force_repaint, boo
 	}
 }
 
-void __stdcall hooks::scene_end() {
+void __stdcall hooks::scene_end() noexcept {
 	static auto original_fn = reinterpret_cast<scene_end_fn>(renderview_hook->get_original(9));
 
 	visuals.chams();
@@ -249,7 +278,7 @@ void __stdcall hooks::scene_end() {
 
 extern LRESULT ImGui_ImplDX9_WndProcHandler(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam);
 
-LRESULT __stdcall hooks::wndproc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) {
+LRESULT __stdcall hooks::wndproc(HWND hwnd, UINT message, WPARAM wparam, LPARAM lparam) noexcept {
 	static bool pressed = false;
 
 	if (!pressed && GetAsyncKeyState(VK_INSERT)) {
@@ -275,7 +304,7 @@ LRESULT __stdcall hooks::wndproc(HWND hwnd, UINT message, WPARAM wparam, LPARAM 
 	return CallWindowProcA(wndproc_original, hwnd, message, wparam, lparam);
 }
 
-void __stdcall hooks::lock_cursor() {
+void __stdcall hooks::lock_cursor() noexcept {
 	static auto original_fn = reinterpret_cast<lock_cursor_fn>(surface_hook->get_original(67));
 
 	if (menu.opened) {
@@ -287,7 +316,7 @@ void __stdcall hooks::lock_cursor() {
 }
 
 static bool initialized = false;
-long __stdcall hooks::present(IDirect3DDevice9* device, RECT* source_rect, RECT* dest_rect, HWND dest_window_override, RGNDATA* dirty_region) {
+long __stdcall hooks::present(IDirect3DDevice9* device, RECT* source_rect, RECT* dest_rect, HWND dest_window_override, RGNDATA* dirty_region) noexcept {
 	if (!initialized) {
 		menu.apply_fonts();
 		menu.setup_resent(device);
@@ -305,7 +334,7 @@ long __stdcall hooks::present(IDirect3DDevice9* device, RECT* source_rect, RECT*
 	return original_present(device, source_rect, dest_rect, dest_window_override, dirty_region);
 }
 
-long __stdcall hooks::reset(IDirect3DDevice9* device, D3DPRESENT_PARAMETERS* present_parameters) {
+long __stdcall hooks::reset(IDirect3DDevice9* device, D3DPRESENT_PARAMETERS* present_parameters) noexcept {
 	if (!initialized)
 		original_reset(device, present_parameters);
 
